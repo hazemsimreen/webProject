92c3eae9af6655831e5e25dae3c9694fe9c65023
Ahmad <ce.ahmadyaser23@gmail.com>
2025-12-15 23:48:34 +0200

    php fixing

--- php/config.php
+++ php/config.php
@@ -2,7 +2,7 @@
 // php/config.php
 
 // Database configuration
-define('DB_HOST', 'localhost');
+define('DB_HOST', '127.0.0.1');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_NAME', 'Meals_Engineers');
