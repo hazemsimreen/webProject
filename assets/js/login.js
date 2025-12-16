ed719594817f1cdddaeac57578c230ec3653ee19
Ahmad <ce.ahmadyaser23@gmail.com>
2025-12-16 08:42:16 +0200

    Sign In php created , deleted unecssary files , fixing id names

--- assets/js/login.js
+++ assets/js/login.js
@@ -3,11 +3,5 @@ document.getElementById('loginForm').addEventListener('submit', function(e) {
     
     const username = document.getElementById('email').value.trim();
     const password = document.getElementById('password').value.trim();
-    
-    if (username === 'admin' && password === 'ahmad') {
-        alert('Login Successful! Redirecting to Admin Dashboard...');
-        window.location.href = 'admin.html';
-    } else {
-        alert('❌ Invalid Username or Password!\nTry: admin / ahmad');
-    }
+
 });
