17b715771f6cc1e176515b3ac3ef228c1ecd704c
Ahmad <ce.ahmadyaser23@gmail.com>
2025-12-16 09:19:07 +0200

    Adding SignIn php file

--- assets/js/login.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('loginForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    
-    const username = document.getElementById('email').value.trim();
-    const password = document.getElementById('password').value.trim();
-
-});
--- php/signIn.php
+++ php/signIn.php
@@ -1,13 +1,44 @@
 <?php
-require_once 'config.php'
+require_once 'config.php';
+
+if($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $stmt = $conn->prepare("SELECT id,password,role FROM users WHERE username = ?");
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+    $result = $stmt->get_result();
 
 
+    if ($result->num_rows == 1)
+    {
+        echo "username exists";
+        $row = $result->fetch_assoc();
+        $hashedPassword = $row["password"];
+
+        if (password_verify($password, $hashedPassword))
+        {
+            echo "upass exists";
+
+            if ($row["role"] == "admin")
+            {
+                header("location: ../admin.html");
+                exit();
+            }
+            else {
+                echo "it should  exists";
+
+                header("location: ../index.html");
+                exit();
+            }
+
+        }
 
 
+    }
 
-
-
-
-
+}
 
 ?>
